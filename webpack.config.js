76dce2fb5afab839cf129eea9c70700ff7b89e71
Artur Augusto Martins <arturaugusto@gmail.com>
2020-04-18 18:24:32 -0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/lisp.js',
+  mode: 'production',
   output: {
     filename: 'lisp.js',
     path: path.resolve(__dirname, 'dist'),
