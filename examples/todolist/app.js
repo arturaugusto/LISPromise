3c73680e4f9440e9dbfc337ddf4e436e570c2b5e
Artur Augusto Martins <arturaugusto@gmail.com>
2020-04-18 20:06:02 -0300

    fix dependencies

--- examples/todolist/app.js
+++ examples/todolist/app.js
@@ -1,5 +1,4 @@
-import wlisp from '../../wlisp.js'
+import wlisp from '../../src/wlisp.js'
 var lsp = new wlisp()
-//var x = lsp.run('(print lala)')
 document.getElementById('root')
   .addEventListener('click', (e) => lsp.fromNode(e.target, 'lisp'))
